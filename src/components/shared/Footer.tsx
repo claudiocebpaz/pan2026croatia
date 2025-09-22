bee8c2903aeac451b388d1ffd4cc00121665e8f9
Claudio Ceballos Paz <claudio@hashdog.com>
2025-09-22 13:40:15 -0300

    chore(deploy): actualización 2025-09-22 16:40:15 UTC

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-950 text-white text-center p-4 mt-auto">
+    <footer className="bg-black text-white text-center p-4 mt-auto">
       <p>&copy; 2025 Project Sanctuary. All rights reserved.</p>
       <p>
         <Link to="/terms" className="font-semibold text-gray-50">
