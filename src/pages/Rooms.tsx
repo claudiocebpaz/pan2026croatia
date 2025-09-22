7d853af606c230b74d879096ccc950f5007137a9
Claudio Ceballos Paz <claudio@hashdog.com>
2025-09-22 13:35:56 -0300

    chore(deploy): actualización 2025-09-22 16:35:56 UTC

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -48,13 +48,13 @@ function Rooms() {
         id="single-rooms"
       />
       <Room
-        roomData={IslandHomeSData}
-        pricingTiers={IslandHomeSData.pricingTiers}
+        roomData={IslandHomeMData}
+        pricingTiers={IslandHomeMData.pricingTiers}
         layoutType="even"
       />
       <Room
-        roomData={IslandHomeMData}
-        pricingTiers={IslandHomeMData.pricingTiers}
+        roomData={IslandHomeSData}
+        pricingTiers={IslandHomeSData.pricingTiers}
       />
       <RoomSeparator
         title="Sea Sunset Glamping Lodges"
